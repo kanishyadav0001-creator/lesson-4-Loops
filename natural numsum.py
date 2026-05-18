fe133cc3ed51660208f828be25a5d5e06f560b47
kanishyadav0001-creator <kanishyadav0001@gmail.com>
2026-05-18 11:05:26 +0530

    initial commit

---
+++ natural numsum.py
@@ -0,0 +1,9 @@
+sum = int(input("Enter natural number"))
+total_sum = 0
+num = 1
+
+while num <= sum:
+    total_sum += num
+    num += 1
+
+print(f"The sum of the first ten natural is {total_sum} ")    
